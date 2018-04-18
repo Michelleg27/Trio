e261448a9def6792c6dc5830b9dfedba67e7690f
michelleg27 <mgomez48@mail.csuchico.edu>
2018-04-17 21:23:25 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+//Author: Michelle Gomez
 #include <iostream>
 using namespace std;
+
+
+//prototypes...
+void maxValue(int& num1, int& num2, int& num3); 
+ 
 
 int main()
 {
@@ -9,7 +15,11 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
+  int num1 = red; 
+  int num2 = green; 
+  int num3 = blue; 
 
+  maxValue(red, green, blue); 
 
 
 
@@ -18,4 +28,40 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+   
+
 }
+
+//definitions...
+void maxValue(int& num1, int& num2, int& num3)
+{
+    if(num1 > num2)
+    {
+        int temp = num1; 
+        num1 = num2; 
+        num2 = temp; 
+    }
+   
+    if(num1 > num3)
+    {
+        int temp = num1; 
+        num1 = num3; 
+        num3 = temp; 
+    }
+  
+    if( num2 > num3)
+    {
+        int temp = num2; 
+        num2 = num3; 
+        num3 = temp; 
+    }
+   
+    if( num3 > num2)
+    {
+        int temp = num3; 
+        num3 = num2; 
+        num2 = temp;
+    }
+    
+}
+
