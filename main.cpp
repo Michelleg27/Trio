75e148234aacb7e4ab46cc5a978f20c2c3298c0b
michelleg27 <mgomez48@mail.csuchico.edu>
2018-04-17 23:35:13 -0700

    fixed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 //prototypes...
 void maxValue(int& num1, int& num2, int& num3); 
+
  
 
 int main()
@@ -15,9 +16,6 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-  int num1 = red; 
-  int num2 = green; 
-  int num3 = blue; 
 
   maxValue(red, green, blue); 
 
@@ -35,33 +33,50 @@ int main()
 //definitions...
 void maxValue(int& num1, int& num2, int& num3)
 {
-    if(num1 > num2)
+    if(num1 > num2 && num1 > num3)
     {
-        int temp = num1; 
-        num1 = num2; 
-        num2 = temp; 
+        int greatest = num1;
+        if(num2 > num3)
+        {
+            int middle = num2;
+            int last = num3; 
+        }
+        else if( num3 > num2)
+        {
+            int middle = num3; 
+            int last = num2; 
+        }
+
     }
-   
-    if(num1 > num3)
+    if(num2 > num1 && num2 > num3)
     {
-        int temp = num1; 
-        num1 = num3; 
-        num3 = temp; 
+        int greatest = num2;
+        if( num1 > num3)
+        {
+            int middle = num1; 
+            int last = num3; 
+        }
+        else if(num3 > num1)
+        {
+            int middle = num3;
+            int last = num1; 
+        }
     }
-  
-    if( num2 > num3)
+    if(num3 > num1 && num3 > num2)
     {
-        int temp = num2; 
-        num2 = num3; 
-        num3 = temp; 
-    }
-   
-    if( num3 > num2)
-    {
-        int temp = num3; 
-        num3 = num2; 
-        num2 = temp;
+        int greatest = num3; 
+        if( num1 > num2)
+        {
+            int middle = num1; 
+            int last = num2; 
+        }
+        else if( num2 > num1)
+        {
+            int middle = num2; 
+            int last = num1; 
+        }
     }
     
+  
 }
 
